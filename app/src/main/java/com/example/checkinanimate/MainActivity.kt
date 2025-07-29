0f17237551a0627abf4938f415e79d5f667472fe
Nguyen Nguyen <nhn22801@gmail.com>
2025-07-29 20:29:29 +0700

    Update animation

--- app/src/main/java/com/example/checkinanimate/MainActivity.kt
+++ app/src/main/java/com/example/checkinanimate/MainActivity.kt
@@ -6,19 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -27,7 +22,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -35,20 +29,17 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathMeasure
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.round
@@ -71,43 +62,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Checkin(modifier: Modifier = Modifier) {
-    val reactState by remember { mutableStateOf(ReactPosition.Start) }
     var textPosition by remember { mutableStateOf(IntOffset.Zero) }
     var reactPosition by remember { mutableStateOf(IntOffset.Zero) }
-    val offsetAnimation: Dp by animateDpAsState(
-        if (reactState == ReactPosition.Start) {
-            0.dp
-        } else {
-            with(LocalDensity.current) { (textPosition.y - reactPosition.y).toDp() }
-        }
-    )
     val animatedDistance = remember { Animatable(0f) }
     val offset = remember { Animatable(0f) }
-    val rotation = remember { Animatable(0f) }
     val alpha = remember { Animatable(1f) }
     val scope = rememberCoroutineScope()
     val localDensity = LocalDensity.current
 
-    val path = Path().apply {
-        moveTo(0f, 0f)
-        cubicTo(100f, 200f, 300f, 0f, 400f, 200f)
-    }
-    val pathProgress by animateFloatAsState(
-        targetValue = 1f,
-        animationSpec = tween(durationMillis = 3000, easing = LinearEasing)
-    )
-    val pathMeasure = PathMeasure()
-//    val currentPosition = remember { mutableStateOf(Offset.Zero) }
-    pathMeasure.setPath(path, false) // Set the path once
-    val pathLength = pathMeasure.length
-//    val pos = FloatArray(2)
-//    pathMeasure.getPosTan(pathMeasure.length * pathProgress, pos, null)
-//    currentPosition.value = Offset(pos[0], pos[1])
-
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
                 .size(500.dp, 500.dp)
@@ -126,35 +89,78 @@ fun Checkin(modifier: Modifier = Modifier) {
                     textPosition = coordinate.positionInRoot().round()
                 })
         }
+
         Box(
-            modifier = Modifier.size(200.dp, 200.dp),
-            contentAlignment = Alignment.Center
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 100.dp),
+            contentAlignment = Alignment.Center,
         ) {
+            val composePath = Path().apply {
+                moveTo(reactPosition.x.toFloat() + 150, reactPosition.y.toFloat() + 150)
+                cubicTo(
+                    reactPosition.x.toFloat() + 700,
+                    reactPosition.y.toFloat() - 500,
+                    textPosition.x.toFloat() + 700,
+                    textPosition.y.toFloat() - 500,
+                    textPosition.x.toFloat(),
+                    textPosition.y.toFloat()
+                )
+            }
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawPath(
+                    path = composePath,
+                    color = Color.Red,
+                    style = Stroke(width = 5f)
+                )
+            }
+
+            val animatedPathMeasure = PathMeasure()
+            animatedPathMeasure.setPath(composePath, false) // Set the path once
+            val animatedPathLength = animatedPathMeasure.length
+            val position = animatedPathMeasure.getPosition(animatedDistance.value)
+            Image(
+                painter = painterResource(id = R.drawable.sushi),
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .size(50.dp, 50.dp)
+//                    .absoluteOffset { IntOffset(position.x.roundToInt(), -position.y.roundToInt()) }
+                    //.absoluteOffset(y = offset.value.dp)
+                    .absoluteOffset(
+                        x = (position.x - (reactPosition.x + 150)).dp,
+                        y = (position.y - (reactPosition.y + 150)).dp
+                    )
+                    .graphicsLayer(alpha = alpha.value)
+            )
+
             Button(
                 contentPadding = PaddingValues(0.dp),
                 modifier = Modifier
                     .clip(CircleShape)
-                    .size(100.dp),
+                    .size(100.dp)
+                    .align(Alignment.BottomCenter)
+                    .onGloballyPositioned { coordinate ->
+                        reactPosition = coordinate.positionInRoot().round()
+                    },
                 onClick = {
-//                    reactState = when (reactState) {
-//                        ReactPosition.Start -> ReactPosition.Finish
-//                        ReactPosition.Finish -> ReactPosition.Start
-//                    }
                     scope.launch {
                         // First animation
                         animatedDistance.animateTo(
-                            targetValue = pathLength,
-                            animationSpec = tween(durationMillis = 2000, easing = LinearEasing)
+                            targetValue = animatedPathLength,
+                            animationSpec = tween(durationMillis = 5000, easing = LinearEasing)
                         )
+                        // straight line
 //                        offset.animateTo(
 //                            targetValue = with(localDensity) { (textPosition.y - reactPosition.y).toDp() }.value,
-//                            animationSpec = tween(1000)
+//                            animationSpec = tween(1000, easing = LinearEasing)
 //                        )
                         // Second animation after the first
                         alpha.animateTo(
                             0f,
-                            animationSpec = tween(500)
+                            animationSpec = tween(100, easing = LinearEasing)
                         )
+                        animatedDistance.snapTo(0f)
                         offset.snapTo(0f)
                         alpha.snapTo(1f)
                     }
@@ -167,32 +173,15 @@ fun Checkin(modifier: Modifier = Modifier) {
                 )
             }
 
-            val positionOffset = pathMeasure.getPosition(animatedDistance.value)
-            val imageX = positionOffset.x
-            val imageY = positionOffset.y
-
-            Image(
-                painter = painterResource(id = R.drawable.sushi),
-                contentDescription = null,
+            Text(
                 modifier = Modifier
-                    .size(50.dp, 50.dp)
-                    .onGloballyPositioned { coordinate ->
-                        reactPosition = coordinate.positionInRoot().round()
-                    }
-                    .alpha(alpha = alpha.value)
-//                    .absoluteOffset(x = currentPosition.value.x.dp, y = currentPosition.value.y.dp)
-                    .absoluteOffset(x = imageX.dp, y = imageY.dp)
+                    .align(Alignment.BottomCenter)
+                    .padding(top = 100.dp),
+                text = "ImageX ${position.x - (reactPosition.x + 150)}" +
+                        " ImageY ${position.y - (reactPosition.y + 150)}"
             )
         }
-        Text(
-            "Text Position: x=${textPosition.x}, y=${textPosition.y} \n" +
-                    "React Position: x=${reactPosition.x}, y=${reactPosition.y})"
-        )
     }
-}
-
-enum class ReactPosition {
-    Start, Finish
 }
 
 @Preview(showBackground = true)
